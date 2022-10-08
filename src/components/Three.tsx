08c74c434963d66b33cc5f0646bc779d80517717
NomuraKohei <seiryu2nd@gmail.com>
2022-10-08 23:28:40 +0900

    feat: 必要なものだけ

--- src/components/Three.tsx
+++ src/components/Three.tsx
@@ -1,27 +1,27 @@
-import { Html, Merged, Stage, useGLTF, useProgress, useTexture } from '@react-three/drei'
-import { Canvas, useFrame, useLoader } from '@react-three/fiber'
-import React, { Suspense, useCallback, useEffect, useMemo, useRef, useState } from 'react'
-import THREE, { Mesh, TextureLoader } from 'three'
-import { GLTFExporter } from 'three/examples/jsm/exporters/GLTFExporter'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import img from '../../public/24647700_s.jpg'
+import { Html, useTexture } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import Image from 'next/image'
+import React, { Suspense, useEffect, useRef, useState } from 'react'
+// import THREE, { Mesh } from 'three'
+// import { GLTFExporter } from 'three/examples/jsm/exporters/GLTFExporter'
 import Controls from '../utils/Controls'
+import gifImg from '/public/loading.gif'
 
-const Thing: React.FC = () => {
-  const ref = useRef({} as Mesh)
-  useFrame(() => (ref.current.rotation.z += 0.01))
-  return (
-    <mesh
-      ref={ref}
-      onClick={(e) => console.log('click')}
-      onPointerOver={(e) => console.log('hover')}
-      onPointerOut={(e) => console.log('unhover')}
-    >
-      <planeBufferGeometry attach='geometry' args={[1, 1]} />
-      <meshBasicMaterial attach='material' color='hotpink' opacity={0.5} transparent />
-    </mesh>
-  )
-}
+// const Thing: React.FC = () => {
+//   const ref = useRef({} as Mesh)
+//   useFrame(() => (ref.current.rotation.z += 0.01))
+//   return (
+//     <mesh
+//       ref={ref}
+//       onClick={(e) => console.log('click')}
+//       onPointerOver={(e) => console.log('hover')}
+//       onPointerOut={(e) => console.log('unhover')}
+//     >
+//       <planeBufferGeometry attach='geometry' args={[1, 1]} />
+//       <meshBasicMaterial attach='material' color='hotpink' opacity={0.5} transparent />
+//     </mesh>
+//   )
+// }
 
 interface RGBColor {
   r: number
@@ -60,128 +60,134 @@ export interface ThreeProps {
   setting?: ObjectPosition
   gltfPath?: 'scene-meshopt' | 'scene2-meshopt'
   isDownload?: boolean
+  initialScene?: boolean
 }
 
 const Three: React.FC<ThreeProps> = (props) => {
   const meshRef = useRef(null)
-  const [currentLink, setLink] = useState<HTMLAnchorElement | undefined>(undefined)
+  // const [currentLink, setLink] = useState<HTMLAnchorElement | undefined>(undefined)
 
-  const save = useCallback(
-    (blob: any, filename: any) => {
-      if (!currentLink) {
-        return
-      }
-      currentLink.href = URL.createObjectURL(blob)
-      currentLink.download = filename
-      currentLink.click()
-    },
-    [currentLink],
-  )
+  // const save = useCallback(
+  //   (blob: any, filename: any) => {
+  //     if (!currentLink) {
+  //       return
+  //     }
+  //     currentLink.href = URL.createObjectURL(blob)
+  //     currentLink.download = filename
+  //     currentLink.click()
+  //   },
+  //   [currentLink],
+  // )
 
-  const saveArrayBuffer = useCallback(
-    (buffer: any, filename: any) => {
-      save(new Blob([buffer], { type: 'application/octet-stream' }), filename)
-    },
-    [save, meshRef, meshRef.current],
-  )
+  // const saveArrayBuffer = useCallback(
+  //   (buffer: any, filename: any) => {
+  //     save(new Blob([buffer], { type: 'application/octet-stream' }), filename)
+  //   },
+  //   [save, meshRef, meshRef.current],
+  // )
 
-  const saveString = useCallback(
-    (text: any, filename: any) => {
-      save(new Blob([text], { type: 'text/plain' }), filename)
-    },
-    [save, meshRef, meshRef.current],
-  )
+  // const saveString = useCallback(
+  //   (text: any, filename: any) => {
+  //     save(new Blob([text], { type: 'text/plain' }), filename)
+  //   },
+  //   [save, meshRef, meshRef.current],
+  // )
 
-  useEffect(() => {
-    if (!props.isDownload) {
-      return
-    }
-    if (typeof window === 'object') {
-      const link = document.createElement('a')
-      document.body.appendChild(link) // Firefox workaround, see #6594
-      setLink(link)
-      console.log(link)
-    }
-  }, [meshRef, meshRef.current])
+  // useEffect(() => {
+  //   if (!props.isDownload) {
+  //     return
+  //   }
+  //   if (typeof window === 'object') {
+  //     const link = document.createElement('a')
+  //     document.body.appendChild(link) // Firefox workaround, see #6594
+  //     setLink(link)
+  //     console.log(link)
+  //   }
+  // }, [meshRef, meshRef.current])
 
-  useEffect(() => {
-    if (!props.isDownload) {
-      return
-    }
-    console.log(currentLink)
-    const exporter = new GLTFExporter()
-    const params = {
-      trs: false,
-      onlyVisible: true,
-      binary: false,
-      maxTextureSize: 4096,
-    }
-    const options = {
-      trs: params.trs,
-      onlyVisible: params.onlyVisible,
-      binary: params.binary,
-      maxTextureSize: params.maxTextureSize,
-    }
-    if (!meshRef.current) {
-      return
-    }
-    exporter.parse(
-      meshRef.current,
-      function (gltf) {
-        console.log(gltf)
-        if (gltf instanceof ArrayBuffer) {
-          saveArrayBuffer(gltf, 'scene.glb')
-        } else {
-          const output = JSON.stringify(gltf, null, 2)
-          console.log(output)
-          saveString(output, 'scene.gltf')
-        }
-      },
-      function (error) {
-        console.log('An error happened during parsing', error)
-      },
-      options,
-    ) // you will have to provide the options here
-  }, [meshRef, meshRef.current, saveArrayBuffer, saveString, currentLink])
+  // useEffect(() => {
+  //   if (!props.isDownload) {
+  //     return
+  //   }
+  //   console.log(currentLink)
+  //   const exporter = new GLTFExporter()
+  //   const params = {
+  //     trs: false,
+  //     onlyVisible: true,
+  //     binary: false,
+  //     maxTextureSize: 4096,
+  //   }
+  //   const options = {
+  //     trs: params.trs,
+  //     onlyVisible: params.onlyVisible,
+  //     binary: params.binary,
+  //     maxTextureSize: params.maxTextureSize,
+  //   }
+  //   if (!meshRef.current) {
+  //     return
+  //   }
+  //   exporter.parse(
+  //     meshRef.current,
+  //     function (gltf) {
+  //       console.log(gltf)
+  //       if (gltf instanceof ArrayBuffer) {
+  //         saveArrayBuffer(gltf, 'scene.glb')
+  //       } else {
+  //         const output = JSON.stringify(gltf, null, 2)
+  //         console.log(output)
+  //         saveString(output, 'scene.gltf')
+  //       }
+  //     },
+  //     function (error) {
+  //       console.log('An error happened during parsing', error)
+  //     },
+  //     options,
+  //   )
+  // }, [meshRef, meshRef.current, saveArrayBuffer, saveString, currentLink])
 
-  const [gltfPath, setGltfPath] = useState(props.gltfPath)
+  // const [gltfPath, setGltfPath] = useState(props.gltfPath)
 
-  useEffect(() => {
-    setGltfPath(props.gltfPath)
-  }, [props.gltfPath])
+  // useEffect(() => {
+  //   setGltfPath(props.gltfPath)
+  // }, [props.gltfPath])
 
-  const Model: React.FC = () => {
-    const gltf = useGLTF(`/${gltfPath}.glb`)
-    return <primitive object={gltf.scene} />
-  }
+  // const Model: React.FC = () => {
+  //   const gltf = useGLTF(`/${gltfPath}.glb`)
+  //   return <primitive object={gltf.scene} />
+  // }
 
   function Loader() {
-    const { progress } = useProgress()
-    return <Html center>{`Please wait...(${progress}%/100%)`}</Html>
-  }
-
-  const [settings, setSettings] = useState(props.setting)
-
-  const Box: React.FC<Pixel> = (props) => {
     return (
-      <mesh
-        position={[
-          settings
-            ? props.position.x / settings.position.x.multiply + settings.position.x.offset
-            : 0,
-          settings
-            ? props.position.y / settings.position.y.multiply + settings.position.y.offset
-            : 0,
-          settings
-            ? props.position.z / settings.position.z.multiply + settings.position.z.offset
-            : 0,
-        ]}
-      >
-        <boxGeometry args={[0.01, 0.01, 0.01]} />
-        <meshStandardMaterial color={`rgb(${props.color.r}, ${props.color.g}, ${props.color.b})`} />
-      </mesh>
+      <Html center>
+        <div style={{ width: 200, height: 150 }}>
+          <Image src={gifImg} alt='loading' width={200} height={150} />
+        </div>
+      </Html>
     )
   }
+
+  // const [settings, setSettings] = useState(props.setting)
+
+  // const Box: React.FC<Pixel> = (props) => {
+  //   return (
+  //     <mesh
+  //       position={[
+  //         settings
+  //           ? props.position.x / settings.position.x.multiply + settings.position.x.offset
+  //           : 0,
+  //         settings
+  //           ? props.position.y / settings.position.y.multiply + settings.position.y.offset
+  //           : 0,
+  //         settings
+  //           ? props.position.z / settings.position.z.multiply + settings.position.z.offset
+  //           : 0,
+  //       ]}
+  //     >
+  //       <boxGeometry args={[0.01, 0.01, 0.01]} />
+  //       <meshStandardMaterial color={`rgb(${props.color.r}, ${props.color.g}, ${props.color.b})`} />
+  //     </mesh>
+  //   )
+  // }
 
   const ImageBox: React.FC = () => {
     const texture = useTexture('/24597263_s.jpg')
@@ -209,6 +215,7 @@ const Three: React.FC<ThreeProps> = (props) => {
 
   const ImageBox2: React.FC = () => {
     const texture = useTexture('/24597263_s.jpg')
+
     return (
       <>
         <mesh position={[-1.5, 0.33, 1.1]}>
@@ -234,47 +241,44 @@ const Three: React.FC<ThreeProps> = (props) => {
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
       <Canvas>
-        <ambientLight intensity={0.1} />
-        <directionalLight color='white' position={[0, 0, 1]} />
-        {props.pixels && props.setting && (
-          <mesh ref={meshRef}>
-            {props.pixels.map((item, index) => {
-              if (item.position.x % 2 === 0 && item.position.y % 2 === 0) {
-                return (
-                  <mesh
-                    position={[
-                      props.setting
-                        ? item.position.x / props.setting.position.x.multiply +
-                          props.setting.position.x.offset
-                        : 0,
-                      props.setting
-                        ? item.position.y / props.setting.position.y.multiply +
-                          props.setting.position.y.offset
-                        : 0,
-                      props.setting
-                        ? item.position.z / props.setting.position.z.multiply +
-                          props.setting.position.z.offset
-                        : 0,
-                    ]}
-                    key={index}
-                  >
-                    <sphereGeometry args={[0.01, 0.01, 0.01]} />
-                    <meshStandardMaterial
-                      color={`rgb(${item.color.r}, ${item.color.g}, ${item.color.b})`}
-                    />
-                  </mesh>
-                )
-              }
-            })}
-            <ImageBox />
-          </mesh>
-        )}
-        {props.gltfPath && (
-          <Suspense fallback={<Loader />}>
-            <Model />
-          </Suspense>
-        )}
-        <Controls />
+        <Suspense fallback={<Loader />}>
+          <ambientLight intensity={0.1} />
+          <directionalLight color='white' position={[0, 0, 1]} />
+          {props.pixels && props.setting && (
+            <mesh ref={meshRef}>
+              {props.pixels.map((item, index) => {
+                if (item.position.x % 2 === 0 && item.position.y % 2 === 0) {
+                  return (
+                    <mesh
+                      position={[
+                        props.setting
+                          ? item.position.x / props.setting.position.x.multiply +
+                            props.setting.position.x.offset
+                          : 0,
+                        props.setting
+                          ? item.position.y / props.setting.position.y.multiply +
+                            props.setting.position.y.offset
+                          : 0,
+                        props.setting
+                          ? item.position.z / props.setting.position.z.multiply +
+                            props.setting.position.z.offset
+                          : 0,
+                      ]}
+                      key={index}
+                    >
+                      <sphereGeometry args={[0.01, 0.01, 0.01]} />
+                      <meshStandardMaterial
+                        color={`rgb(${item.color.r}, ${item.color.g}, ${item.color.b})`}
+                      />
+                    </mesh>
+                  )
+                }
+              })}
+              {props.initialScene ? <ImageBox2 /> : <ImageBox />}
+            </mesh>
+          )}
+          <Controls />
+        </Suspense>
       </Canvas>
     </div>
   )
